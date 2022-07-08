57d9a363c3214a6f71c43be32da6766182116a03
JuVaargawi <108183559+JuVaargawi@users.noreply.github.com>
2022-07-08 18:46:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+song="";
+leftwristx=0;
+leftwristy=0;
+rightwristx=0;
+rightwristy=0;
+
+function preload()
+{
+    song=loadSound("relaxmusic.mp3");
+}
+function setup()
+{
+    canvas=createCanvas(500,500);
+    canvas.center();
+
+    video=createCapture(VIDEO);
+    video.hide();
+
+    posenet=ml5.poseNet(video, modelLoaded);
+    posenet.on('pose',gotPoses);
+}
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+        leftwristx=results[0].pose.leftWrist.x;
+        leftwristy=results[0].pose.leftWrist.y;
+        console.log("leftwristx="+leftwristx,"leftwristy="+leftwristy);
+
+        rightwristx=results[0].pose.rightWrist.x;
+        rightwristy=results[0].pose.rightWrist.y;
+        console.log("rightwristx="+rightwristx,"rightwristy="+rightwristy);
+    }
+}
+
+function modelLoaded()
+{
+    console.log("model is loaded");
+}
+
+function draw()
+{
+    image(video,0,0,500,500);
+}
+function play()
+{
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+
+}
